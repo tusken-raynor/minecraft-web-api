d00a85277b30989075db11211c692092fd825a43
Sam Powell <sammypoo@Texas-Toast.local>
2025-07-29 11:34:02 -0600

    Added say endpoint

--- api/say/index.js
+++ api/say/index.js
@@ -2,12 +2,9 @@ const rcon = require('../../rcon');
 
 module.exports = {
   get(req, res) {
-    // Return html for a simple message submission form
-    const username = req.query.username || '';
     res.send(`
-      <form method="POST" action="/api/msg/">
+      <form method="POST" action="/api/say/">
         ${req.query.success ? '<p>Message sent successfully!</p><br>' : ''}
-        <input type="text" name="username" placeholder="Username" value="${username}" required><br>
         <textarea name="message" placeholder="Enter your message" rows="2" required></textarea><br>
         <input type="hidden" name="redirect" value="1">
         <button type="submit">Send</button>
@@ -24,19 +21,13 @@ module.exports = {
     if (!message) {
       return res.status(400).send({ success: false, message: 'Message is required' });
     }
-    let username = req.body.username || 'Anonymous';
 
-    // Escape quotes and backslashes in the message
-    message = message.replace(/"/g, '\\"').replace(/\\/g, '\\\\');
-    // Escape quotes in the username
-    username = username.replace(/"/g, '\\"').replace(/\\/g, '\\\\');
-
-    rcon.send(`tellraw @a {"text":"<${username}> ${message}","color":"white"}`);
+    rcon.send(`say ${message}`);
 
     const doRedirect = req.body.redirect === '1';
     if (doRedirect) {
       // Redirect back to the form with a success message
-      res.redirect(`/api/msg/?username=${encodeURIComponent(username)}&success=1`);
+      res.redirect(`/api/say/?success=1`);
     } else {
       // Respond with a success message
       res.send({ success: true, message: 'Message sent successfully' });
