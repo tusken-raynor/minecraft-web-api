c681a1fa9caa22c264334a2125a4e9e441f23369
Sam Powell <sammypoo@Texas-Toast.local>
2025-08-04 17:07:00 -0600

    Added messaging to accurately indicate if the whitelist command worked

--- api/whitelist/remove/index.js
+++ api/whitelist/remove/index.js
@@ -4,16 +4,9 @@ module.exports = {
   get(req, res) {
     // Return html for a simple whitelist remove form
     const username = req.query.username || '';
-    res.send(`
-      <form method="POST" action="/api/whitelist/remove/">
-        ${req.query.success ? '<p>Player removed from whitelist successfully!</p><br>' : ''}
-        <input type="text" name="username" placeholder="Username" value="${username}" required><br>
-        <input type="hidden" name="redirect" value="1">
-        <button type="submit">Remove from Whitelist</button>
-      </form>
-    `);
+    res.send(htmlForm('', username));
   },
-  post(req, res) {
+  async post(req, res) {
     if (!rcon.connected()) {
       return res.status(503).send({ success: false, message: 'RCON not connected' });
     }
@@ -27,15 +20,27 @@ module.exports = {
     // Escape quotes and backslashes in the username
     username = username.replace(/"/g, '\\"').replace(/\\/g, '\\\\');
 
-    rcon.send(`whitelist remove ${username}`);
+    const response = await rcon.send(`whitelist remove ${username}`);
 
     const doRedirect = req.body.redirect === '1';
     if (doRedirect) {
-      // Redirect back to the form with a success message
-      res.redirect(`/api/whitelist/remove/?username=${encodeURIComponent(username)}&success=1`);
+      // Reload the form with a success message
+      res.send(htmlForm(response));
     } else {
       // Respond with a success message
       res.send({ success: true, message: 'Player removed from whitelist successfully' });
     }
   }
+}
+
+
+function htmlForm(message = '', username = '') {
+  return `
+    <form method="POST" action="/api/whitelist/remove/">
+      ${message ? `<p>${message}</p><br>` : ''}
+      <input type="text" name="username" placeholder="Username" value="${username}" required><br>
+      <input type="hidden" name="redirect" value="1">
+      <button type="submit">Remove from Whitelist</button>
+    </form>
+  `;
 }
