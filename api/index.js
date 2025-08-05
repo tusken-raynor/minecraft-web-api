fdc084506064b00aa4fb5a22e90ce4b35256392c
Sam Powell <sammypoo@Texas-Toast.local>
2025-08-05 10:19:46 -0600

    Added test for endpoint module importing

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const msg = require("./msg");
 const players = require("./players");
 const playtime = require("./players/playtime");
@@ -16,4 +17,36 @@ module.exports = {
   whitelist: whitelist,
   'whitelist/add': whitelistAdd,
   'whitelist/remove': whitelistRemove,
+}
+
+module.exports.getEndPoints = function() {
+  // Get the paths to all the index.js files recursively nested in the ./api using fs
+  const apiDir = `${__dirname}/api`;
+
+  const apiEndpointPaths = getAPIEndpointPaths(apiDir);
+
+  const endpoints = {};
+  apiEndpointPaths.forEach(path => {
+    const endpointName = path.replace(apiDir + '/', '/api/').replace('/index.js', '');
+    const endpointModule = require(path);
+    endpoints[endpointName] = endpointModule;
+  });
+
+  return endpoints;
+}
+
+function getAPIEndpointPaths(dir) {
+  const paths = [];
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const fullPath = `${dir}/${file}`;
+    if (fs.statSync(fullPath).isDirectory()) {
+      paths.push(...getAPIEndpointPaths(fullPath)); // Recursively get paths from subdirectories
+    }
+    else if (file === 'index.js') {
+      paths.push(fullPath); // Add the index.js file path
+    }
+  }
+    
+  return paths;
 }
